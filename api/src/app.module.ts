623887276710f4c1ec70574e17093da1f977a235
Hector Ramírez <heckramirezc@aol.com>
2025-10-17 02:59:04 -0600

    Agregando conexión de base de datos a API

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,42 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DataSource } from 'typeorm'; 
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'production' 
+        ? '../.env.production'
+        : '../.env', 
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT', 5432),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: false,
+        
+        ssl: configService.get<string>('DB_SSL_MODE') === 'require' 
+          ? { rejectUnauthorized: false }
+          : false, 
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+    constructor(private dataSource: DataSource) {
+    }
+}
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DataSource } from 'typeorm';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  try {
+    const dataSource = app.get(DataSource);
+    if (dataSource.isInitialized) {
+      console.log('API Service: Conexión a la base de datos establecida correctamente.');
+    } else {
+      console.error('API Service: Error: La conexión a la base de datos no se inicializó.');
+    }
+  } catch (error) {
+    console.error('API Service: Error al intentar obtener la conexión a la base de datos:', error.message);
+  }
+
+  await app.listen(process.env.PORT || 3000);
+  console.log(`API Service: Aplicación NestJS corriendo en: ${await app.getUrl()}`);
 }
 bootstrap();
